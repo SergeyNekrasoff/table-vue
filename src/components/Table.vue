5235cb04c6e4880f106297d66fa44707b15563d0
unknown <snekrasov@hculture.ru>
2020-06-09 13:26:24 +0400

    add sorting

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,12 +3,16 @@
         <table class="table">
             <thead>
                 <tr class="table__row table__row--head">
-                    <td><b>Dessert(100g serving)</b></td>
-                    <td>Calories</td>
-                    <td>Fat (g)</td>
-                    <td>Carbs (g)</td>
-                    <td>Protein (g)</td>
-                    <td>Iron (%)</td>
+                    <td 
+                        @click="setSort('name')" 
+                        :class="['td', { 'td--sort': sorting === 'name' }]">
+                        <b>Dessert(100g serving)</b>
+                    </td>
+                    <td @click="setSort('cal')">Calories</td>
+                    <td @click="setSort('fat')">Fat (g)</td>
+                    <td @click="setSort('carbs')">Carbs (g)</td>
+                    <td @click="setSort('protein')">Protein (g)</td>
+                    <td @click="setSort('iron')">Iron (%)</td>
                 </tr>    
             </thead>
             <tbody>
@@ -57,6 +61,13 @@ export default {
                 currentPage: data.currentPage
             })
         },
+        setSort(value) {
+            if (this.sorting === value) {
+                this.sorting = ''
+            } else {
+                this.sorting = value
+            }
+        },
         ...mapActions(['getItems'])
     },
     watch: {
@@ -70,6 +81,14 @@ export default {
         },
         'nextPage': function(value) {
             this.currentPage = value
+        },
+        'sorting': function(oldVal, newVal) {
+            if (oldVal !== newVal) {
+                this.updatePagination({
+                    pages: this.pages,
+                    currentPage: 1
+                })
+            }
         }
     },
     computed: {
@@ -78,7 +97,51 @@ export default {
         }),
         completeList() {
             const index = this.currentPage * this.rowsLength - this.rowsLength
-            return this.items.slice(index, index + this.rowsLength)
+
+            if (this.sorting === 'calories') {
+                const sortArr = this.items.slice().sort((a, b) => a.calories.value - b.calories.value)
+                return sortArr.slice(index, index + this.rowsLength)
+            } else if (this.sorting === 'fat') {
+                const sortArr = this.items.slice().sort((a, b) => {
+                    const num1 = Math.ceil(a.fat.value) * 100 / 100
+                    const num2 = Math.ceil(b.fat.value) * 100 / 100
+                    return num1 - num2
+                })
+                return sortArr.slice(index, index + this.rowsLength)
+            } else if (this.sorting === 'carbs') {
+                const sortArr = this.items.slice().sort((a, b) => {
+                    const num1 = Math.ceil(a.carbs.value) * 100 / 100
+                    const num2 = Math.ceil(b.carbs.value) * 100 / 100
+                    return num1 - num2
+                })
+                return sortArr.slice(index, index + this.rowsLength)
+            } else if (this.sorting === 'protein') {
+                const sortArr = this.items.slice().sort((a, b) => {
+                    const num1 = Math.ceil(a.protein.value) * 100 / 100
+                    const num2 = Math.ceil(b.protein.value) * 100 / 100
+                    return num1 - num2
+                })
+                return sortArr.slice(index, index + this.rowsLength)
+            } else if (this.sorting === 'iron') {
+                const sortArr = this.items.slice().sort((a, b) => {
+                    const num1 = Math.ceil(a.iron.value) * 100 / 100
+                    const num2 = Math.ceil(b.iron.value) * 100 / 100
+                    return num1 - num2
+                })
+                return sortArr.slice(index, index + this.rowsLength)
+            } else if (this.sorting === 'name') {
+                const sortArr = this.items.slice().sort((a, b) => {
+                    const num1 = a.name
+                    const num2 = b.name
+
+                    if (num1 == num2) return 0
+                    if (num1 > num2) return 1
+                    if (num1 < num2) return -1
+                })
+                return sortArr.slice(index, index + this.rowsLength)
+            } else {
+                return this.items.slice(index, index + this.rowsLength)
+            }
         }
     },
     mounted() {
@@ -113,10 +176,29 @@ export default {
             background: #ccc;
             cursor: pointer;
         }
+
+        &--head td {
+            cursor: pointer;
+        }
     }
 
     td {
         border-bottom: 1px solid #ddd;
+    }
+
+    .td {
+        position: relative;
+        &:before {
+            content: '↓';
+            font-size: 16px;
+            color: #ccc;
+            position: relative;
+            left: 170px;
+        }
+        
+        &--sort:before {
+            content: '↑';
+        }
     }
 }
 </style>
